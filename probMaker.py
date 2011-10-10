a33e1a8cdac3e463700f8058d26e7b08b5a6d714
Jonathan Varkovitzky <vark@uw.edu>
2011-10-10 10:02:33 -0700

    plotting of h now working

--- probMaker.py
+++ probMaker.py
@@ -9,25 +9,24 @@ from pylab import *
 event1 = loadtxt('fort.fg01_0020',skiprows=9)
 
 
-mu = (76,32,88,120)
+mu = 100
 n = 200
 m = 200
 
 #Need to take the column of inundaiton data and convert it into a rectangular matrix
 h = zeros((n,m))
-for i in range (1,n):
-    for j in range (1,m):
-       h[i,j] = event1[i+j-1,4]
+for i in range (0,n-1):
+    for j in range (0,m-1 ):
+       h[i,j] = event1[i*n+j,3] #CHECK THESE INDICIES!!!
   
 
 muMap = zeros((n,m))
 
-for k in range(0,1):              #Loop through invidual events
-    for i in range(0,n):          #Loop through x direction
-        for j in range(0,m):      #Loop through y direction
-            if eventk[i,j,k] > 1:
-                muMap[i,j] = muMap[i,j] + mu[k]
+for i in range(1,n):          #Loop through x direction
+    for j in range(1,m):      #Loop through y direction
+        if h[i,j] > 0.0:
+            muMap[i,j] = muMap[i,j] + mu
 
 
-matplotlib.pyplot.contour(muMap,100)
+matplotlib.pyplot.contour(h,100)
 show()
