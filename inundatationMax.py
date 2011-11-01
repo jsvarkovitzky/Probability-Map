fixed indicies and added P computation, doesnt look right yet

inundatationMax.py
@@ -5,13 +5,13 @@
 from numpy import *
 from matplotlib import *
 from pylab import *
-
+from scipy.special import *
 #inundationMax()
 
 ########################################################
 ## Iterate over each time step to find max inundation ##
 ########################################################
-def maxH( nfiles,n,m ):
+def maxH( nfiles,n,m,k ):
     fnums = range(1,nfiles)
     print "Reading in fort.q**** files"
 
@@ -27,7 +27,7 @@ def maxH( nfiles,n,m ):
         max_h2 = zeros((n,m))
     
     print "Analysing frames"
-    for k in range(1,nfiles):
+    for k in range(0,nfiles):
         varnam = 'fort_%s' %str(k).zfill(4)
         exec('y = %s[:,1].copy()'%varname)
         z = y.reshape(n,m,order='F')
@@ -36,8 +36,8 @@ def maxH( nfiles,n,m ):
             z1 = z
             
         #print 'Analysing frame # %s' %str(k).zfill(4)
-        for i in range(1,n):
-            for j in range(1,m):
+        for i in range(0,n):
+            for j in range(0,m):
                 if  z[i,j] < 10**20 and z[i,j]>max_h[i,j]:
                      max_h[i,j] = z[i,j]
                         
@@ -46,46 +46,57 @@ def maxH( nfiles,n,m ):
 ################################################
 ## Take max_h information and generate P_{ij} ##
 ################################################
-def tidalUncert(zeta,zeta_i,fieldType,n,m):
+def tidalUncert(zeta,zeta_i,fieldType,n,m,k,nu,T):
     print "Computing P(zeta>zeta_i)"
+    
     #Constant values obtained from Mofjeld et al. 2007
     MSL = 1.130
     MHHW = 2.095
     alpha = 0.170
     beta = 0.858
     C = 1.044
-    CPrime = 0.707 
-    alphaPrime = 0.056
-    betaPrime = 1.119
+    CP = 0.707 
+    alphaP = 0.056
+    betaP = 1.119
     sigma_0 = 0.638 
     
     #calculate the probability of excedence at each point (i,j)
-    for i in range(1,n):
-        for j in range(1,m): 
-            zeta_0 = zeta[i,j] + MSL + C*(MHHW-MSL)*exp(-alpha*(zeta[i,j]/sigma_0)**betaPrime)
-
-    return
+    mu = zeros((n,m))
+    P = zeros((n,m))
+    for t in range(0,k):
+        for i in range(0,n):
+            for j in range(0,m): 
+                #equations from Gonzalez et al 2009
+                zeta_0 = zeta[i,j] + MSL + C*(MHHW-MSL)*exp(-alpha*(zeta[i,j]/sigma_0)**betaP)
+                sigma = sigma_0-CP*sigma_0*exp(-alphaP*(zeta[i,j]/sigma_0))**betaP
+                mu[i,j] = mu[i,j] + 1./2*nu[t]*(1-erf((zeta[i,j]-zeta_0)/(sqrt(2)*sigma)))
+    P = ones((n,m))-exp(-1*mu*T)
+    return(P,mu)
 
 
 ##################
 ## Program Main ##
 ##################
 
-nfiles = 6
-((n,m)) = ((271,192)) #x-pts, y-pts ### in the future automate this!!!!!
+nfiles = 59
+((n,m,k)) = ((271,192,1)) #x-pts, y-pts, k-timesteps ### in the future automate this!!!!!
+T_M = 520                 #recurance time from Gonzalez et al. 2009
+T = 100                   #period of intrest 
+nu = ones((1,k))*1./T_M
+
 #compute raw max height from simulation
-max_h = maxH(nfiles,n,m)
+max_h = maxH(nfiles,n,m,k)
 #compute P_{ij}
-zeta_i = 1              #meters of inundation
+zeta_i = 0.0              #meters of inundation
 fieldType = 1           #1 = far-field, 2 = near-field
-P = tidalUncert(max_h,zeta_i,fieldType,n,m)
+(P,mu) = tidalUncert(max_h,zeta_i,fieldType,n,m,k,nu,T)
 
 #########################
 ## Plotting Algorithms ##
 #########################
 
 figure(3)
-pcolormesh(max_h)
+pcolormesh(P)
 colorbar()
 show()
 