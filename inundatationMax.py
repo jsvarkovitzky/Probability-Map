839bcb0f1403f18a42c78016b39a60fb478af407
Jonathan Varkovitzky <vark@uw.edu>
2011-11-06 15:49:42 -0800

    changed variables to match Gonzalez et al

--- inundatationMax.py
+++ inundatationMax.py
@@ -24,9 +24,9 @@ def maxH( nfiles,n,m,k ):
         exec('%s=loadtxt(%r, skiprows=9)'%(varname,fname))
         status(fnum,nfiles)
         max_h = zeros((n,m))
-        max_h2 = zeros((n,m))
-    
-    print "Analysing frames"
+       
+    #print "Analysing frames"
+    print "Analysing Frames: "
     for k in range(0,nfiles):
         status(k,nfiles)
         varnam = 'fort_%s' %str(k).zfill(4)
@@ -36,7 +36,7 @@ def maxH( nfiles,n,m,k ):
         if k == 1:
             z1 = z
             
-        #print 'Analysing frame # %s' %str(k).zfill(4)
+        #check and correct max_h against frame k
         for i in range(0,n):
             for j in range(0,m):
                 if  z[i,j] < 10**20 and z[i,j]>max_h[i,j]:
@@ -47,7 +47,7 @@ def maxH( nfiles,n,m,k ):
 ################################################
 ## Take max_h information and generate P_{ij} ##
 ################################################
-def tidalUncert(zeta,zeta_i,fieldType,n,m,k,nu,T):
+def tidalUncert(eta,zeta_i,fieldType,nx,ny,runs,nu,T):
     print "Computing P(zeta>zeta_i)"
     
     #Constant values obtained from Mofjeld et al. 2007
@@ -62,22 +62,20 @@ def tidalUncert(zeta,zeta_i,fieldType,n,m,k,nu,T):
     sigma_0 = 0.638 
     
     #calculate the probability of excedence at each point (i,j)
-    mu = zeros((n,m))
-    P = zeros((n,m))
-    #diff = zeros((n,m))
-    for s in range(0,k):         #loop through events
-        for i in range(0,n):     #loop through x
-            for j in range(0,m): #loop through y
-                #equations from Gonzalez et al 2009
-                zeta_0 = zeta[i,j] + MSL + C*(MHHW-MSL)*exp(-alpha*(zeta[i,j]/sigma_0)**beta)
-                sigma = sigma_0-CP*sigma_0*exp(-1*alphaP*(zeta[i,j]/sigma_0)**betaP)
-                mu[i,j] = 1./2*nu[s]*(1-erf((zeta_i-zeta[i,j])/(sqrt(2)*sigma)))
-                print "(i,j) = (%s,%r)" %(i,j)
-                print "The vale of zeta_0 is %s" %(zeta_0)
-                print "The value of sigma is %s" %(sigma)
-                print "The value of mu[i,j] is %s" %(mu[i,j])
-    P = ones((n,m))-exp(-1*mu*T)
-    return(P,mu,diff)
+    mu = zeros((nx,ny))
+    P = zeros((nx,ny))
+    
+    print 'Computing Probability: '
+    for s in range(0,runs):         #loop through events
+        for ix in range(0,nx):     #loop through x
+            status(ix,nx)
+            for iy in range(0,ny): #loop through y
+                #The following equations are from Gonzalez et al 2009
+                zeta_0j = eta[ix,iy] + MSL + C*(MHHW-MSL)*exp(-alpha*(eta[ix,iy]/sigma_0)**beta) #eqn 2b
+                sigma_j = sigma_0-CP*sigma_0*exp(-1*alphaP*(eta[ix,iy]/sigma_0)**betaP)          #eqn 2c
+                mu[ix,iy] = mu[ix,iy] + 1./2*nu[s]*(1-erf((zeta_i-zeta_0j)/(sqrt(2)*sigma_j)))   #eqn 3b/5
+                P = ones((nx,ny))-exp(-1*mu*T)                                                   #eqn 6
+    return(P,mu)
 
 ####################
 ## Plotting Tools ##
@@ -96,28 +94,37 @@ def status(n,N):
     n = float(n)
     N = float(N)
     percent = n/N*100
-    sys.stdout.write("[==> ]%3d%%\r" % percent)
+    sys.stdout.write("[==> ]%3d%%\r" %percent)
     sys.stdout.flush()
-
+    
 ##################
 ## Program Main ##
 ##################
 
-nfiles = 59
-((n,m,k)) = ((271,192,1)) #x-pts, y-pts, k-timesteps ### in the future automate this!!!!!
-#((n,m,k)) = ((2,2,1))
+#test = 0 implies to run code on full set of  inundations
+#test = 1 implies to run a test on a 2x2 identity matrix instead of inundation
+test = 1
+
+if test == 0:
+    nfiles = 59
+    ((nx,ny,runs)) = ((271,192,1)) #x-pts, y-pts, k-timesteps ### in the future automate this!!!!!
+    #compute raw max height from simulation
+    max_h = maxH(nfiles,n,m,k)
+    
+
+if test == 1:
+     ((nx,ny,runs)) = ((2,2,1))
+     max_h = array([[1,0],[0,1]])*100
+
+
 T_M = 520                 #recurance time from Gonzalez et al. 2009
-T = 100                   #period of intrest 
-nu = ones((1,k))*1./T_M
+T = 1                   #period of intrest 
+nu = ones((1,runs))*1./T_M
 
-#compute raw max height from simulation
-max_h = maxH(nfiles,n,m,k)
-
-#max_h = array([[1,0],[0,1]])
 #compute P_{ij}
-zeta_i = 0.0              #meters of inundation
+zeta_i = 2.1            #meters of inundation
 fieldType = 1           #1 = far-field, 2 = near-field
-(P,mu,diff) = tidalUncert(max_h,zeta_i,fieldType,n,m,k,nu,T)
+(P,mu) = tidalUncert(max_h,zeta_i,fieldType,nx,ny,runs,nu,T)
 
 #Calls plotting algorithms
 plotting()
