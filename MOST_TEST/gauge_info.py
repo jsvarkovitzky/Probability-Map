b022d9fecd722974da1fc7552678ddf2ac7cddfd
Jonathan Varkovitzky <vark@uw.edu>
2011-11-07 13:48:00 -0800

    adding gauge_info.py

---
+++ MOST_TEST/gauge_info.py
@@ -0,0 +1,7 @@
+gauge_info = []
+#gauge_info.append([224.73, 57.02,1])   # x position, y position, gauge ID
+#gauge_info.append([224.61, 57.025,2])   # x position, y position, gauge ID
+#gauge_info.append([224.65, 57.025,3])   # x position, y position, gauge ID
+#gauge_info.append([224.73, 57.075,4])   # x position, y position, gauge ID
+#gauge_info.append([224.71, 57.08,5])   # x position, y position, gauge ID
+#gauge_info.append([224.61, 57.075,6])   # x position, y position, gauge ID
